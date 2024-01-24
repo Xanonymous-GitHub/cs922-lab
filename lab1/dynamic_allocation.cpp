6d6456e2ac131534c30736ee80d495dc3a7226bf
Xanonymous <trusaidlin@gmail.com>
2024-01-24 11:16:28 +0000

    refactor: 💡 make dynamic input safer

--- lab1/dynamic_allocation.cpp
+++ lab1/dynamic_allocation.cpp
@@ -4,20 +4,22 @@
 
 #define SIZE 5
 
-int main(int argc, char const *argv[]) {
-    std::array<int, SIZE> arr;
-    int temp;
+int main(int argc, char const* argv[]) {
+    std::array<int, SIZE> arr = {};
+
+    std::string input = "";
     int i = 0;
-    
+
     // Read in the value
     // s until user enters nothing
-    while (std::cin >> temp) {
-        arr[i++] = temp;
+    while (i < SIZE && std::getline(std::cin, input) && !input.empty()) {
+        arr[i++] = std::stoi(input);
     }
 
-    i--;
-    for (; i >= 0; i--) {
-        std::cout << arr[i] << '\n';
+    auto it = arr.rbegin();
+    it += SIZE - i;
+    for (; it != arr.rend(); ++it) {
+        std::cout << *it << std::endl;
     }
 
     return 0;
