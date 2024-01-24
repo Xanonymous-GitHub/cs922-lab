c566cbea9c1ac6bb95252e20bc3683ee9d4c53be
Xanonymous <trusaidlin@gmail.com>
2024-01-24 16:24:50 +0000

    fix: 🐛 ensure executable path is absolute path

--- kudu-runcode.bash
+++ kudu-runcode.bash
@@ -4,6 +4,7 @@
 function run_job() {
     # Default job name prefix
     local default_job_prefix="¡job_"
+    local executable=""
 
     # Get the job name from command line arguments
     while [ "$#" -gt 0 ]; do
@@ -25,9 +26,12 @@ function run_job() {
         job_name="${default_job_prefix}$(date -u +%s | sha256sum | cut -c1-8)"
     fi
 
-    # Check if executable is provided and non-empty
+    # Convert the executable path to an absolute path
     if [ -z "$executable" ]; then
         echo "Error: Executable name is required."
+        return 1
+    elif ! executable=$(realpath "$executable"); then
+        echo "Error: The executable '$executable' does not exist or cannot be accessed."
         return 1
     fi
 
