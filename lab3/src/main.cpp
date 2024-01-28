aa66129ff27a3ce368621a69f4c7bf2c22ea9307
Xanonymous <trusaidlin@gmail.com>
2024-01-28 03:11:13 +0000

    refactor: 💡 rewrite main file

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -1,28 +1,27 @@
+#include "Driver.hpp"
+#include "InputFile.hpp"
+
 #include <cstdlib>
 #include <iostream>
 #include <string>
 
-#include "Driver.hpp"
-#include "InputFile.hpp"
-
-int main(int argc, char *argv[]) {
+int main(const int& argc, const char* argv[]) {
     if (argc != 2) {
         std::cerr << "Usage: deqn <filename>" << std::endl;
-        exit(1);
+        std::exit(1);
     }
 
-    const char *filename = argv[1];
-    InputFile input(filename);
+    const char* filename = argv[1];
 
+    const InputFile input{filename};
     std::string problem_name(filename);
 
-    int len = problem_name.length();
-
-    if (problem_name.substr(len - 3, 3) == ".in")
+    if (const auto len = problem_name.length(); problem_name.substr(len - 3, 3) == ".in") {
         problem_name = problem_name.substr(0, len - 3);
+    }
 
     // Strip out leading path
-    size_t last_sep = problem_name.find_last_of("/");
+    size_t last_sep = problem_name.find_last_of('/');
 
     if (last_sep != std::string::npos) {
         last_sep = last_sep + 1;
@@ -32,7 +31,7 @@ int main(int argc, char *argv[]) {
 
     problem_name = problem_name.substr(last_sep, problem_name.size());
 
-    Driver driver(&input, problem_name);
+    Driver driver{input, problem_name};
 
     driver.run();
 
