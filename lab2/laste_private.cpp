f75991404c4e2892fb937b6c608fd3b2a02c0710
Xanonymous <trusaidlin@gmail.com>
2024-01-29 22:54:15 +0000

    test: 💍 try using shared()

--- lab2/laste_private.cpp
+++ lab2/laste_private.cpp
@@ -8,12 +8,12 @@ int main(const int argc, char const *argv[]) {
     omp_set_dynamic(false);
     omp_set_num_threads(4);
 
-#pragma omp parallel
+#pragma omp parallel shared(a)
     {
 #pragma omp single
         std::cout << "number of thread: " << omp_get_num_threads() << '\n';
 
-#pragma omp for private(a)
+#pragma omp for
         for (int i = 0; i < 10; ++i) {
 #pragma omp critical
             std::cout
